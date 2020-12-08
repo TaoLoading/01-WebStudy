f08641346833e0588ad4377d268c8eb9a6a7ac8d
TaoLoading <1377226380@qq.com>
2020-12-08 12:38:00 +0800

    delete 00-test

--- 00-test/node_modules/.bin/ejs.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\ejs\bin\cli.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\ejs\bin\cli.js" %*
-)
--- 00-test/node_modules/.bin/jake.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\jake\bin\cli.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\jake\bin\cli.js" %*
-)
