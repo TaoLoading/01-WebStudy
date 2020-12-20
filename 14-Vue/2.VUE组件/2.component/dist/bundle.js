fbfcf2bd358ef26dc97fd89d7eee708f18e57f9e
TaoLoading <1377226380@qq.com>
2020-12-20 16:23:23 +0800

    组件

---
+++ 14-Vue/2.VUE组件/2.component/dist/bundle.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+eval("console.log('Hello Webpack!')\r\ndocument.getElementById('root').innerHTML = '<h1>Hello222</h1>'\r\n\n\n//# sourceURL=webpack://2.component/./src/index.js?");
+/******/ })()
+;
---
+++ 14-Vue/2.VUE组件/2.component/src/index.js
@@ -0,0 +1,2 @@
+console.log('Hello Webpack!')
+document.getElementById('root').innerHTML = '<h1>Hello222</h1>'
