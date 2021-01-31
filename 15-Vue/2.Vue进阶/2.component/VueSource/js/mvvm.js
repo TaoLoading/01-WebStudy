374a9acdc86bc4b70da574bfb4e7d9b9d60390d5
TaoLoading <1377226380@qq.com>
2021-01-31 20:47:35 +0800

    数据代理补充

--- 15-Vue/2.Vue进阶/2.component/VueSource/js/mvvm.js
+++ 15-Vue/2.Vue进阶/2.component/VueSource/js/mvvm.js
@@ -1,35 +1,47 @@
+/*
+相当于Vue的构造函数
+*/
+
 function MVVM(options) {
-    this.$options = options;
-    var data = this._data = this.$options.data;
-    var me = this;
+	// 将配置对象保存到vm上
+	this.$options = options
+	// 将data对象保存到vm和变量data上
+	var data = (this._data = this.$options.data)
+	// 将vm保存到变量me上
+	var me = this
 
-    // 数据代理
-    // 实现 vm.xxx -> vm._data.xxx
-    Object.keys(data).forEach(function(key) {
-        me._proxy(key);
-    });
+	// 遍历data的每个属性
+	Object.keys(data).forEach(function(key) {
+		// 实现指定属性的数据代理
+		me._proxy(key)
+	})
 
-    observe(data, this);
+	observe(data, this)
 
-    this.$compile = new Compile(options.el || document.body, this)
+	this.$compile = new Compile(options.el || document.body, this)
 }
 
 MVVM.prototype = {
-    $watch: function(key, cb, options) {
-        new Watcher(this, key, cb);
-    },
+	$watch: function(key, cb, options) {
+		new Watcher(this, key, cb)
+	},
 
-    _proxy: function(key) {
-        var me = this;
-        Object.defineProperty(me, key, {
-            configurable: false,
-            enumerable: true,
-            get: function proxyGetter() {
-                return me._data[key];
-            },
-            set: function proxySetter(newVal) {
-                me._data[key] = newVal;
-            }
-        });
-    }
-};
+	_proxy: function(key) {
+		var me = this
+		// 给vm定义指定属性名的属性
+		Object.defineProperty(me, key, {
+			configurable: false,
+			enumerable: true,
+			// 当vm.name读取数据时自动调用
+			get: function proxyGetter() {
+				// 读取并返回data中对应的属性值
+				return me._data[key]
+			},
+			// 当vm.name=value设置数据时自动调用
+			set: function proxySetter(newVal) {
+				// 将最新的值保存到data对应的属性上
+				me._data[key] = newVal
+			},
+		})
+	},
+}
