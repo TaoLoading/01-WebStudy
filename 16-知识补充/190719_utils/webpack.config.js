5fc6db19ee1668c4d04c089949715c1a7e3bcaad
TaoLoading <1377226380@qq.com>
2021-06-17 10:34:51 +0800

    修改路径

--- 16-知识补充/190719_utils/webpack.config.js
+++ 16-知识补充/190719_utils/webpack.config.js
@@ -13,7 +13,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '190719-utils.js',
     // filename: '190719-utils.min.js',
-    library: 'aUtils', // 向外暴露的对象的名称
-    libraryTarget: 'umd', // 针对 esm / commonjs
+    // 向外暴露的对象的名称
+    library: 'aUtils',
+    // 打包生成的库可以通过esm、commonjs、requirejs的语法引入
+    libraryTarget: 'umd',
+  },
+
+  // 配置引入的外部包
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      // 页面引入的全局变量
+      root: '_'
+    }
   }
 }
