ccf14af332257f459c26080e209429109ad9bda1
TaoLoading <1377226380@qq.com>
2021-06-30 18:06:05 +0800

    初始化文件

--- 15-Vue/5.TS和Vue3/vue3/vue3-basic/src/App.vue
+++ 15-Vue/5.TS和Vue3/vue3/vue3-basic/src/App.vue
@@ -1,27 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h2>{{count}}</h2>
+  <button @click="updateCount()">更新数据</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent, ref } from "vue";
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  // 新的option, 所有的组合API函数都在此使用, 只在初始化时执行一次
+  setup() {
+    // let count = 0; // 此时数据并不是响应式数据
+    // ref定义一个数据的响应式。创建一个包含响应式数据的引用对象，通过xxx.value形式修改数据
+    const count = ref(0);
+    function updateCount() {
+      count.value++;
+    }
+    // 返回一个对象
+    return {
+      count,
+      updateCount,
+    };
+  },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ 15-Vue/5.TS和Vue3/vue3/vue3-basic/初始化文件/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo"
+       src="./assets/logo.png">
+  <!-- 使用子级组件 -->
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+// defineComponent函数，目的是定义一个组件
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+// 向外暴露定义好的组件，内部传入配置对象
+export default defineComponent({
+  // 组件名
+  name: "App",
+  // 注册组件
+  components: {
+    // 注册子组件
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
