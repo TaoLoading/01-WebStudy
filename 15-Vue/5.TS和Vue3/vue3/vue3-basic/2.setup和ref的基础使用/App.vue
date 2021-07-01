633fc83a3064f4c86236722a5cd66cf06575e79a
TaoLoading <1377226380@qq.com>
2021-07-01 10:08:14 +0800

    路径修改

--- 15-Vue/5.TS和Vue3/vue3/vue3-basic/2.setup和ref的基础使用/App.vue
+++ 15-Vue/5.TS和Vue3/vue3/vue3-basic/2.setup和ref的基础使用/App.vue
@@ -8,7 +8,7 @@ export default defineComponent({
   name: "App",
   // 新的option, 所有的组合API函数都在此使用, 只在初始化时执行一次
   setup() {
-    // let count = 0; // 此时数据并不是响应式数据
+    // let count = 0; // 不能进行修改，因为此时数据并不是响应式数据
     // ref定义一个基本类型的响应式数据。返回一个包含响应式数据的引用对象，通过xxx.value形式修改数据
     // 在html模板中不需要使用.value获取数据
     const count = ref(0);
