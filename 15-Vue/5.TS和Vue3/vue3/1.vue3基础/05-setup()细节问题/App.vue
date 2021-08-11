7230b0eee3edc431e977d37e08fc5b1ea6338b29
TaoLoading <1377226380@qq.com>
2021-08-11 09:20:33 +0800

    修改路径

---
+++ 15-Vue/5.TS和Vue3/vue3/1.vue3基础/05-setup()细节问题/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{msg}}</div>
+  <button @click="msg += '==='">更新数据</button>
+  <hr>
+  <child :msg="msg"
+         @xxx="xxx"></child>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Child from "./components/Child.vue";
+export default defineComponent({
+  components: { Child },
+  name: "App",
+  setup() {
+    const msg = ref("this is a msg");
+    function xxx() {
+      console.log(msg.value);
+    }
+    // 返回一个对象
+    return {
+      msg,
+      xxx,
+    };
+  },
+});
+</script>
