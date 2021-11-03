6395ff2b179b7420b5037e05f81813c68cdeb3f0
TaoLoading <1377226380@qq.com>
2021-11-03 10:58:31 +0800

    文件夹迁移

--- 16-知识补充/03-虚拟DOM与diff算法/02-diff/src/dom/patchVnode.js
+++ 16-知识补充/03-虚拟DOM与diff算法/02-diff/src/dom/patchVnode.js
@@ -2,28 +2,28 @@
  * 新老节点为同一个节点时处理情况
  */
 
-import createElement from "./createElement"
-import updateChildren from "./updateChildren"
-
-export default function patchVnode(oldVnode, newVnode) {
-  // 判断新节点是否有子节点
-  if (newVnode.children == undefined) {
-    if (newVnode.text !== oldVnode.text) {
-      // 当新节点没有子节点并且文本不相同时，将老节点的文本替换为新节点的文本
-      oldVnode.elm.innerText = newVnode.text
-    }
-  } else {
-    // 判断老节点是否有子节点
-    if (oldVnode.children !== undefined && oldVnode.children.length > 0) {
-      // diff核心(最复杂)
-      updateChildren(oldVnode.elm, oldVnode.children, newVnode.children)
-    } else {
-      // 老节点没有子节点时，将老节点清空并对新节点进行添加
-      oldVnode.elm.innerHTML = ''
-      for (let item of newVnode.children) {
-        let childDom = createElement(item)
-        oldVnode.elm.appendChild(childDom)
-      }
-    }
-  }
-}
+ import createElement from "./createElement"
+ import updateChildren from "./updateChildren"
+ 
+ export default function patchVnode(oldVnode, newVnode) {
+   // 判断新节点是否有子节点
+   if (newVnode.children == undefined) {
+     if (newVnode.text !== oldVnode.text) {
+       // 当新节点没有子节点并且文本不相同时，将老节点的文本替换为新节点的文本
+       oldVnode.elm.innerText = newVnode.text
+     }
+   } else {
+     // 判断老节点是否有子节点
+     if (oldVnode.children !== undefined && oldVnode.children.length > 0) {
+       // diff核心(最复杂)
+       updateChildren(oldVnode.elm, oldVnode.children, newVnode.children)
+     } else {
+       // 老节点没有子节点时，将老节点清空并完成新节点的添加
+       oldVnode.elm.innerHTML = ''
+       for (let item of newVnode.children) {
+         let childDom = createElement(item)
+         oldVnode.elm.appendChild(childDom)
+       }
+     }
+   }
+ }
---
+++ 16-知识补充/README.md
@@ -0,0 +1,2 @@
+# 文件夹迁移
+## 此部分为手写代码，由于文件夹迁移此部分不在更新，最新代码在仓库[08-Algorithm](https://github.com/TaoLoading/08-Algorithm)中
