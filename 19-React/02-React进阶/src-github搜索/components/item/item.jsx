3c7f5294364bb8d138258e447401ccdd10bf03a5
TaoLoading <1377226380@qq.com>
2021-12-01 14:39:34 +0800

    整理文件结构

---
+++ 19-React/02-React进阶/src-github搜索/components/item/item.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './item.css'
+
+export default class Item extends Component {
+  render() {
+    let { html_url, avatar_url, login } = this.props
+    return (
+      <div className="card">
+        <a href={html_url}>
+          <img src={avatar_url} style={{ width: '100px' }} alt="用户头像" />
+        </a>
+        <p className="card-text">{login}</p>
+      </div>
+    )
+  }
+}
