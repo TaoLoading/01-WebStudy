8f9459024f0ff9a3377cff2e84286b2b2847db71
TaoLoading <1377226380@qq.com>
2021-12-02 15:25:27 +0800

    完善文档

--- 19-React/02-React进阶/src-github搜索(pubsub版)/App.js
+++ 19-React/02-React进阶/src-github搜索(pubsub版)/App.js
@@ -3,6 +3,12 @@ import Search from './components/search/search.jsx'
 import List from './components/list/list.jsx'
 
 export default class App extends Component {
+  /**
+   * 利用PubSub实现消息订阅发布机制
+   * 订阅：PubSub.subscribe('消息名', () => {})
+   * 发布：PubSub.publish('消息名', data)
+   */
+
   render() {
     return (
       <div className="container">
