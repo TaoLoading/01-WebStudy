7f92b6f82afb51c321224dc8e9254bf77fbb7f99
TaoLoading <1377226380@qq.com>
2021-12-02 17:54:47 +0800

    路由学习

--- 19-React/02-React进阶/src/App.js
+++ 19-React/02-React进阶/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
-import Search from './components/search/search.jsx'
-import List from './components/list/list.jsx'
+import { NavLink, BrowserRouter } from 'react-router-dom'
 
 export default class App extends Component {
+  /**
+   * 1.安装react-router：npm i react-router-dom
+   */
+
   render() {
     return (
-      <div className="container">
-        <section className="jumbotron">
-          <h3 className="jumbotron-heading">搜索github用户</h3>
-          {/* search组件 */}
-          <Search />
-        </section>
-        {/* list组件 */}
-        <List />
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <h2>React Router Demo</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              <BrowserRouter>
+                <NavLink className="list-group-item" to="/about" activeClassName="demo">About</NavLink>
+                <NavLink className="list-group-item" to="/home" activeClassName="demo">Home</NavLink>
+              </BrowserRouter>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>我是About的内容</h3>
+                <h3>我是Home的内容</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
