7bd71ebcfd59ef5d337a556b9c4d37cac8569ca9
TaoLoading <1377226380@qq.com>
2021-12-13 16:35:42 +0800

    新增组件

--- 19-React/02-React进阶/02-ts版/README.md
+++
@@ -1,9 +0,0 @@
-# 笔记
-
-## 1.CSS引用
-1. 直接引入整个CSS文件
-   * `import './index.css'`
-   * `<div className = 'app' />`
-2. JSS模块化引入组件
-   * `import style from './index.css'`
-   * `<div className = {styles.app} />`
--- 19-React/02-React进阶/02-ts版/src/App.tsx
+++ 19-React/02-React进阶/02-ts版/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import logo from "./assets/images/logo.svg";
 import robots from './mockdata/robots.json'
 import Robot from "./components/Robot";
+import ShoppingCart from "./components/ShoppingCart";
 import styles from './App.module.css'
 
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人</h1>
+      </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
         {robots.map((r) => {
           return <Robot key={r.id} id={r.id} name={r.name} email={r.email}></Robot>
