a34e3862c0619c3473446d1f602d023dcd3c0656
TaoLoading <1377226380@qq.com>
2021-12-13 16:50:16 +0800

    替换数据源

--- 19-React/02-React进阶/02-ts版/src/App.tsx
+++ 19-React/02-React进阶/02-ts版/src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react'
+
 import logo from "./assets/images/logo.svg";
 import robots from './mockdata/robots.json'
 import Robot from "./components/Robot";
 import ShoppingCart from "./components/ShoppingCart";
 import styles from './App.module.css'
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <h1>罗伯特机器人</h1>
-      </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map((r) => {
-          return <Robot key={r.id} id={r.id} name={r.name} email={r.email}></Robot>
-        })}
-      </div>
-    </div>
-  );
+interface Props { }
+
+interface State {
+  robotGallery: any[]
 }
 
-export default App;
+export default class App extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robotGallery: []
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => this.setState({ robotGallery: data }))
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>罗伯特机器人</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => {
+            return <Robot key={r.id} id={r.id} name={r.name} email={r.email}></Robot>
+          })}
+        </div>
+      </div>
+    )
+  }
+}
