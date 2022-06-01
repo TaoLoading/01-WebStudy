c6f01737861e841c576f2de49e1f7b7073cf4fa1
TaoLoading <1377226380@qq.com>
2022-06-01 14:34:27 +0800

    复习Vue3

--- 15-Vue/05-TS和Vue3/vue3/06-ref和reactive的细节问题/App.vue
+++ 15-Vue/05-TS和Vue3/vue3/06-ref和reactive的细节问题/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <h3>m1:{{m1}}</h3>
-  <h3>m2:{{m2}}</h3>
-  <h3>m3:{{m3}}</h3>
-  <hr>
+  <h3>m1:{{ m1 }}</h3>
+  <h3>m2:{{ m2 }}</h3>
+  <h3>m3:{{ m3 }}</h3>
+  <hr />
   <button @click="update">更新数据</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from "vue";
+import { defineComponent, reactive, ref } from 'vue'
 export default defineComponent({
   /**
    * ref用来处理基本类型数据, reactive用来处理对象(递归深度响应式)
@@ -16,35 +16,35 @@ export default defineComponent({
    * reactive内部: 通过使用Proxy来实现对对象内部所有数据的劫持, 并通过Reflect操作对象内部数据
    * ref的数据操作: 在js中要.value, 在模板中不需要(内部解析模板时会自动添加.value)
    */
-  name: "App",
+  name: 'App',
   setup() {
-    const m1 = ref("abc");
+    const m1 = ref('abc')
     const m2 = reactive({
-      name: "lht",
+      name: 'lht',
       wife: {
-        name: "xcy",
-      },
-    });
+        name: 'xcy'
+      }
+    })
     // ref中如果放入对象，那么会经reactive处理，形成一个proxy类型的对象
     const m3 = ref({
-      name: "lht",
+      name: 'lht',
       wife: {
-        name: "xcy",
-      },
-    });
+        name: 'xcy'
+      }
+    })
     const update = () => {
-      m1.value += "===";
-      m2.wife.name += "===";
-      m3.value.wife.name += "===";
-      console.log(m2);
-      console.log(m3.value.wife);
-    };
+      m1.value += '==='
+      m2.wife.name += '==='
+      m3.value.wife.name += '==='
+      console.log(m2)
+      console.log(m3.value.wife)
+    }
     return {
       m1,
       m2,
       m3,
-      update,
-    };
-  },
-});
+      update
+    }
+  }
+})
 </script>
