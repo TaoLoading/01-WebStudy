d3bc01ad50e01a936e912b60e044598249cf1333
TaoLoading <1377226380@qq.com>
2022-09-21 11:57:14 +0800

    异步action

---
+++ 18-React/02-React进阶/12-Redux/03-异步action/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Count from './count'
+
+export default function App() {
+  return (
+    <Count />
+  )
+}
---
+++ 18-React/02-React进阶/12-Redux/03-异步action/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import store from './redux/store'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+// 检测store的改变，改变时渲染页面
+// 因为redux只负责管理状态，不进行页面的渲染
+store.subscribe(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
