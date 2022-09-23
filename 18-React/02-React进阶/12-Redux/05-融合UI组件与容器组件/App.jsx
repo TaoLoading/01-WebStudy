824dd088b35931b9dff0154204237992bd2e3c8b
TaoLoading <1377226380@qq.com>
2022-09-23 15:20:19 +0800

    修正代码

--- 18-React/02-React进阶/12-Redux/05-融合UI组件与容器组件/App.jsx
+++ 18-React/02-React进阶/12-Redux/05-融合UI组件与容器组件/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import store from './redux/store'
 import Count from './containers/Count'
 
 export default function App() {
   return (
-    <Count store={store} />
+    <Count />
   )
 }
